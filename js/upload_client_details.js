25a3347726cc0cb25245c5e95baffcb67e9b7425
shubham <shubham99bisht@gmail.com>
2020-06-23 11:34:16 +0530

    Form completed

--- js/upload_client_details.js
+++ js/upload_client_details.js
@@ -1,4 +1,7 @@
 function upload_receipt(){
+
+  swal("Please Wait!", "Uploading Payment Receipt...", "info", {buttons:false, closeOnClickOutside: false});
+
    var file = document.getElementById('payment').files[0];
    var clientname = document.getElementById("entry.1338680096").value.split(" ").join("_");
    if(file!=null && clientname!=""){
@@ -22,6 +25,9 @@ function upload_receipt(){
 
 
 function upload_aadhar(){
+
+  swal("Please Wait!", "Uploading Aadhar Card...", "info", {buttons:false, closeOnClickOutside: false});
+
    var file = document.getElementById('aadhar').files[0];
    var clientname = document.getElementById("entry.1338680096").value.split(" ").join("_");
    if(file!=null && clientname!=""){
@@ -35,6 +41,21 @@ function upload_aadhar(){
          upload_receipt()
        });
      });
-   }
- else{alert("Please upload Aadhar Card in PDF or Image format and fill all details"); location.reload()}
+    }
+   else{alert("Please upload Aadhar Card in PDF or Image format and fill all details"); location.reload()}
+}
+
+
+function form_validation(){
+
+  swal("Please Wait!", "Verifying Details...", "info", {buttons:false, closeOnClickOutside: false});
+
+  id_list = ["entry.1338680096","entry.887837918", "entry.908527243", "entry.1512288643", "entry.336357637", "entry.210865936", "entry.2129207113", "entry.871169116", "entry.1323477373", "entry.2126663962", "entry.1510962497", "entry.1367477446", "entry.56802443", "entry.1876387829"]
+
+  var i;
+  for (i = 0; i < id_list.length; i++) {
+    if(document.getElementById(id_list[i]).value == ""){swal("Feild Empty", "Please Fill all details.", "error"); return false;}
+  }
+
+  if(i==id_list.length){upload_aadhar();}
 }
